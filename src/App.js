91c2ffa09c78b3189817a45ecc2321866f310a56
Mandar Kulkarni <mandar1308@gmail.com>
2021-04-29 12:21:27 +0530

    statusbar style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React, { useState } from 'react';
-import { LogBox } from 'react-native';
+import { LogBox, StatusBar } from 'react-native';
 
 import Navigator from './navigation';
 import { NavigationService } from './utilities';
@@ -10,11 +10,19 @@ const App = () => {
   let data = new Map();
   data.set('user', {});
   data.set('cart', []);
+
   const [context, setContext] = useState(data);
+
   LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
   LogBox.ignoreAllLogs(true);
+
   return (
     <Context.Provider value={[context, setContext]}>
+      <StatusBar
+        barStyle="dark-content"
+        hidden={false}
+        backgroundColor="rgba(76, 154, 42, 1)"
+      />
       <Navigator
         ref={r => {
           NavigationService.setTopLevelNavigator(r);
